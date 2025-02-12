e254b7cadcbb9a8fb66e8478458fbf3947c54a43
Aditya Naresh <adityanaresh007@gmail.com>
2025-02-12 19:57:51 +0530

    Island added moving on to add sky

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import  {Home, About, Projects, Contact} from './components/pages'
+import  {Home, About, Projects, Contact} from './pages'
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
